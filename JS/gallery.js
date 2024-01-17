0e6f626a7170ad4cb2649b7331495232b826d605
YimingSpyro <64062739+YimingSpyro@users.noreply.github.com>
2024-01-17 19:11:00 +0800

    Update gallery.js

--- JS/gallery.js
+++ JS/gallery.js
@@ -1,35 +1,132 @@
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
-}
+const html = document.querySelector('html');
+html.setAttribute('data-bs-theme', 'dark');
 
-function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-}
+document.addEventListener('DOMContentLoaded', () => {
+  // --- Create LightBox
+  const galleryGrid = document.querySelector(".gallery-grid");
+  const links = galleryGrid.querySelectorAll("a");
+  const imgs = galleryGrid.querySelectorAll("img");
+  const lightboxModal = document.getElementById("lightbox-modal");
+  const bsModal = new bootstrap.Modal(lightboxModal);
+  const modalBody = lightboxModal.querySelector(".lightbox-content");
 
-var slideIndex = 1;
-showSlides(slideIndex);
+  function createCaption (caption) {
+    return `<div class="carousel-caption d-none d-md-block">
+        <h4 class="m-0">${caption}</h4>
+      </div>`;
+  }
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
+  function createIndicators (img) {
+    let markup = "", i, len;
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+    const countSlides = links.length;
+    const parentCol = img.closest('.col');
+    const curIndex = [...parentCol.parentElement.children].indexOf(parentCol);
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    for (i = 0, len = countSlides; i < len; i++) {
+      markup += `
+        <button type="button" data-bs-target="#lightboxCarousel"
+          data-bs-slide-to="${i}"
+          ${i === curIndex ? 'class="active" aria-current="true"' : ''}
+          aria-label="Slide ${i + 1}">
+        </button>`;
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+
+    return markup;
+  }
+
+  function createSlides (img) {
+    let markup = "";
+    const currentImgSrc = img.closest('.gallery-item').getAttribute("href");
+
+    for (const img of imgs) {
+      const imgSrc = img.closest('.gallery-item').getAttribute("href");
+      const imgAlt = img.getAttribute("alt");
+
+      markup += `
+        <div class="carousel-item${currentImgSrc === imgSrc ? " active" : ""}">
+          <img class="d-block img-fluid w-100" src=${imgSrc} alt="${imgAlt}">
+          ${imgAlt ? createCaption(imgAlt) : ""}
+        </div>`;
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
+
+    return markup;
+  }
+
+  function createCarousel (img) {
+    const markup = `
+      <!-- Lightbox Carousel -->
+      <div id="lightboxCarousel" class="carousel slide carousel-fade" data-bs-ride="true">
+        <!-- Indicators/dots -->
+        <div class="carousel-indicators">
+          ${createIndicators(img)}
+        </div>
+        <!-- Wrapper for Slides -->
+        <div class="carousel-inner justify-content-center mx-auto">
+          ${createSlides(img)}
+        </div>
+        <!-- Controls/icons -->
+        <button class="carousel-control-prev" type="button" data-bs-target="#lightboxCarousel" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#lightboxCarousel" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      `;
+
+    modalBody.innerHTML = markup;
+  }
+
+  for (const link of links) {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const currentImg = link.querySelector("img");
+      const lightboxCarousel = document.getElementById("lightboxCarousel");
+
+      if (lightboxCarousel) {
+        const parentCol = link.closest('.col');
+        const index = [...parentCol.parentElement.children].indexOf(parentCol);
+
+        const bsCarousel = new bootstrap.Carousel(lightboxCarousel);
+        bsCarousel.to(index);
+      } else {
+        createCarousel(currentImg);
+      }
+
+      bsModal.show();
+    });
+  }
+
+  // --- Support Fullscreen
+  const fsEnlarge = document.querySelector(".btn-fullscreen-enlarge");
+  const fsExit = document.querySelector(".btn-fullscreen-exit");
+
+  function enterFS () {
+    lightboxModal.requestFullscreen().then({}).catch(err => {
+      alert(`Error attempting to enable full-screen mode: ${err.message} (${err.name})`);
+    });
+    fsEnlarge.classList.toggle("d-none");
+    fsExit.classList.toggle("d-none");
+  }
+
+  function exitFS () {
+    document.exitFullscreen();
+    fsExit.classList.toggle("d-none");
+    fsEnlarge.classList.toggle("d-none");
+  }
+
+  fsEnlarge.addEventListener("click", (e) => {
+    e.preventDefault();
+    enterFS();
+  });
+
+  fsExit.addEventListener("click", (e) => {
+    e.preventDefault();
+    exitFS();
+  });
+})
+
+
